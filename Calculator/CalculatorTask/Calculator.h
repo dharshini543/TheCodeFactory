a4c84f497ef8c573d400430519d480d5f95484d4
Dharshini <dharshinitr1998@gmail.com>
2025-05-20 14:55:24 +0530

    trial

--- Calculator/CalculatorTask/Calculator.h
+++ Calculator/CalculatorTask/Calculator.h
@@ -5,6 +5,15 @@ class Calculator
 {
 public:
     Calculator();
+    int add(int num1, int num2);
+    int sub(int num1, int num2);
+    int mul(int num1, int num2);
+    int div(int num1, int num2);
+    int mod(int num1, int num2);
+
+private:
+    int m_num1;
+    int m_num2;
 };
 
 #endif // CALCULATOR_H
