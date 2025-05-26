218a106d8c291d748a94db92841bfbe323c23fcd
Prathik <prathiksatish7135@gmail.com>
2025-05-26 10:18:31 +0530

    valus changed

--- Calculator/CalculatorTask/main.cpp
+++ Calculator/CalculatorTask/main.cpp
@@ -6,9 +6,9 @@ int main()
 {
     Calculator calc;
     calc.add(20 , 10);
-    calc.sub(20 , 10);
-    calc.mul(20 , 10);
-    calc.div(20 , 10);
+    calc.sub(30 , 10);
+    calc.mul(40 , 10);
+    calc.div(50 , 10);
     calc.mod(20 , 10);
     return 0;
 }
