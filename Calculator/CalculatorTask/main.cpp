c840d33945fa73d1da9c26088d2173e954490597
Prathik <prathiksatish7135@gmail.com>
2025-05-27 10:42:03 +0530

    deleted prouser file

--- Calculator/CalculatorTask/main.cpp
+++ Calculator/CalculatorTask/main.cpp
@@ -7,7 +7,7 @@ int main()
     Calculator calc;
     calc.add(20 , 10);
     calc.sub(30 , 10);
-    calc.mul(40 , 10);
+    calc.mul(10 , 10);
     calc.div(50 , 10);
     calc.mod(80 , 10);
     return 0;
